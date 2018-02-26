7003e97be706ccbd700fe30f74c6f453a382802f
MaxiStarovoitov <rackezik@gmail.com>
2018-02-26 18:14:49 +0300

    solution.Task 3

--- src/index.js
+++ src/index.js
@@ -1,35 +1,45 @@
 class Sorter {
   constructor() {
     this.elems = [];
-    this.comp = function (l, r) { return l-r; };
+    this.compare = function (a, b) { return a - b; };
   }
 
   add(e) { this.elems.push(e); }
+
   at(i) { return this.elems[i]; }
+
   get length() { return this.elems.length; }
 
   toArray() { return this.elems; }
 
+
   sort(indices) {
     var arr = [];
-
-    indices.sort(this.comp);
 
     for (var i = 0; i < indices.length; i++) {
       arr.push(this.elems[indices[i]]);
     }
 
-    arr.sort(this.comp);
+    arr.sort(this.compare);
+
+    if (indices[0] < indices[1]) {
+      for (var i = 0; i < indices.length; i++) {
+        this.elems.splice(indices[i], 1, arr[i]);
+      }
+    } else { indices.reverse(); }
 
     for (var i = 0; i < indices.length; i++) {
       this.elems.splice(indices[i], 1, arr[i]);
     }
-
-    return this.elems;
   }
 
-  setComparator(comp) {
-    this.comp = comp;
+
+  setComparator(compareF) {
+    if (compareF) {
+      return this.compare = compareF;
+    } else {
+      return this.compare;
+    }
   }
 }
 
